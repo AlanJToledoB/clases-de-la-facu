f544e23c3dbd2c8a6238eb8ccc9e8de5f8c9ade4
Alan Joel Toledo Barrientos <107812467+AlanJToledoB@users.noreply.github.com>
2025-09-13 18:56:29 -0300

    Add files via upload

---
+++ POO/ejer 5 u3.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+struct punto{
+	float x;
+	float y;
+};
+
+class rectaExplicita{
+private:
+	float m,b;
+	
+public:
+	
+	rectaExplicita(punto p, punto q){
+		m = (q.y - p.y)/(q.x - p.x);
+		
+		b = p.y - m*p.x;
+	}
+	float verM(){
+		return m;
+	}
+	
+	float verB(){
+		return b;
+	}
+	string obtenerEcuacion(){
+		string mString = to_string(m);
+		string bString = to_string(b);
+		
+		string ecuacion = mString + " x " + " + " +bString;
+		
+		return ecuacion;
+	}
+	
+	bool pertenece(punto p){
+		
+		if(m*p.x + b == p.y){
+			return true;
+		}else{
+			return false;
+		}
+	}
+		
+	//ver cual seria la forma de esto con el del float
+	
+};
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
